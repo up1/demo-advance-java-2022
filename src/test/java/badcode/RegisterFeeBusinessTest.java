0f54dd83c84baf121212ef388d90c3d2e40dd274
up1 <somkiat.p@gmail.com>
2022-02-17 16:37:06 +0700

    Working with textblock

--- src/test/java/badcode/RegisterFeeBusinessTest.java
+++ src/test/java/badcode/RegisterFeeBusinessTest.java
@@ -13,6 +13,7 @@ class RegisterFeeBusinessTest {
             "0,500",
             "1,500",
             "2,250",
+            "5,100",
             "9,50",
             "10,0",
     })
@@ -20,5 +21,27 @@ class RegisterFeeBusinessTest {
         RegisterBusiness business = new RegisterBusiness();
         int actualFee = business.getFee(expYear);
         assertEquals(expectedFee, actualFee);
+    }
+
+    @ParameterizedTest
+    @CsvSource(useHeadersInDisplayName = true, textBlock = """
+            Year,   Expected Result
+            0,      500
+            1,      500
+    """)
+    void getFee2(int expYear, int expectedFee) {
+        RegisterBusiness business = new RegisterBusiness();
+        int actualFee = business.getFee(expYear);
+        assertEquals(expectedFee, actualFee);
+
+        String input = "sdfsdf" +
+                "sdfsdf";
+
+        String input2 = """
+                dfgdfgkdfgjdfkgdjfg
+                dfgdfgdfg
+                   dfgdfgdfgdfg
+                """;
+
     }
 }
