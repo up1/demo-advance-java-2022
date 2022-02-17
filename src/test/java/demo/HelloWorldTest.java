a61f70d7b3362646430bfeb465ca250b1eb28e59
up1 <somkiat.p@gmail.com>
2022-02-17 11:05:27 +0700

    Introduce with test structure

--- src/test/java/demo/HelloWorldTest.java
+++ src/test/java/demo/HelloWorldTest.java
@@ -3,11 +3,19 @@ package demo;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class HelloWorldTest {
 
     @Test
     @DisplayName("เรากำลังทดสอบเรื่องอะไร อย่างไร ต้องได้อะไร")
     public void case01() {
+        // Arrange, Given
+        HelloWorld helloWorld = new HelloWorld();
+        // Act, When
+        String actualResult = helloWorld.hi("Somkiat");
+        // Assert / verify, Then
+        assertEquals("Hello Somkiat", actualResult);
     }
 
     @Test
