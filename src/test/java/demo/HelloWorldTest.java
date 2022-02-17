74df6845d932b2db8a538f602d1e68ede3cd8e9b
up1 <somkiat.p@gmail.com>
2022-02-17 11:00:31 +0700

    Create first test case

--- src/test/java/demo/HelloWorldTest.java
+++ src/test/java/demo/HelloWorldTest.java
@@ -1,11 +1,17 @@
 package demo;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
-    public void hi() {
+    @DisplayName("เรากำลังทดสอบเรื่องอะไร อย่างไร ต้องได้อะไร")
+    public void case01() {
+    }
+
+    @Test
+    public void case02() {
     }
 
 }
